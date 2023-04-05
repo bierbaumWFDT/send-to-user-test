752c0fa9923b4587a91e6f94f407a15fe97f7a7d
Bierbaum <qbierbaum@widgetfinancial.com>
2023-04-05 12:47:57 -0400

    Add spring boot project files.

---
+++ src/main/java/com/widgetfinancial/userwebsocket/UserwebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.widgetfinancial.userwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserwebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserwebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/widgetfinancial/userwebsocket/UserwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widgetfinancial.userwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
